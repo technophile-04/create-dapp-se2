dccc7a25cd60723a56e3df9c266f652e340150d1
Shiv Bhonde <shivbhonde04@gmail.com>
2023-04-29 12:58:51 +0530

    setup directory structure

---
+++ templates/extensions/graph/nextjs/template.config.ts
@@ -0,0 +1,13 @@
+const templateConfig = {
+  _appImports: [
+    "import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'",
+  ],
+  _appOutsideComponentCode: `
+  const subgraphUri = "http://localhost:8000/subgraphs/name/scaffold-eth/your-contract";
+  const apolloClient = new ApolloClient({
+    uri: subgraphUri,
+    cache: new InMemoryCache(),
+  });
+  `,
+  _appProviderWrappers: ["<ApolloProvider client={apolloClient}>"],
+};
--- templates/solidityFrameworks/hardhat/packages/hardhat/test/YouContract.ts
+++ templates/solidityFrameworks/hardhat/packages/hardhat/test/YouContract.ts
@@ -19,7 +19,7 @@ describe("YourContract", function () {
     });
 
     it("Should allow setting a new message", async function () {
-      const newGreeting = "Learn Scaffold-ETH 2! :)";
+      const newGreeting = "Learn scaffold-eth! :)";
 
       await yourContract.setGreeting(newGreeting);
       expect(await yourContract.greeting()).to.equal(newGreeting);
